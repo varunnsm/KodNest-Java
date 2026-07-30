1940b9647927eb1408809966290df6b84765e105
VARUN S M <varunsm15@gmail.com>
2026-07-30 13:02:57 +0530

    add one more line

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,8 +1,11 @@
+
 public class WelcomeToJava {
+
     public static void main(String[] args) {
-       //Write your code here
-       System.out.println("Welcome to the Java Track.");
-       System.out.println("I already know how to build programming logic.");
-       System.out.println("Now I am learning how to express that logic using Java.");
+        //Write your code here
+        System.out.println("Welcome to the Java Track.");
+        System.out.println("I already know how to build programming logic.");
+        System.out.println("Now I am learning how to express that logic using Java.");
+        System.out.println("Now I know how to use git and github");
     }
 }
