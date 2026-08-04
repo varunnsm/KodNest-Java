089e56254ca05645e06011da09d3acfe3f44a6fb
VARUN S M <varunsm15@gmail.com>
2026-08-04 14:36:03 +0530

    build learner profile

---
+++ track/M02/T01/ST01/P03.java
@@ -0,0 +1,10 @@
+
+public class P03 {
+
+    public static void main(String[] args) {
+        String learnerName = "Salman";
+        int dailyPracticeHours = 8;
+        System.out.println("Name: " + learnerName);
+        System.out.println("Daily Practice: " + dailyPracticeHours + " Hours");
+    }
+}
