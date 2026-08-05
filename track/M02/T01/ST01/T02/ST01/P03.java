4927cd634c846f4b4362675ffca89ab7111c73b7
VARUN S M <varunsm15@gmail.com>
2026-08-05 12:12:59 +0530

    Placement Eligibility Checker

---
+++ track/M02/T01/ST01/T02/ST01/P03.java
@@ -0,0 +1,15 @@
+
+public class P03 {
+
+    public static void main(String[] args) {
+        int marks = 72;
+        int attendance = 80;
+        boolean projectCompleted = true;
+
+        boolean markEligible = marks >= 60;
+        boolean attendanceEligible = attendance >= 75;
+        boolean academicEligible = markEligible && attendanceEligible;
+        boolean placementEligible = academicEligible && projectCompleted;
+        System.err.println("Placement Eligible: " + placementEligible);
+    }
+}
