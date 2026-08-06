efe9e83fd533e222575ed8f8e24bd5d61883a2a3
VARUN S M <varunsm15@gmail.com>
2026-08-06 11:06:48 +0530

    do while loop(print 1 to 5)

--- track/M02/T01/ST01/T02/ST01/P08.java
+++ track/M02/T01/ST01/T02/ST01/P08.java
@@ -1,12 +1,12 @@
-//while loop(print 1 to 5)
+//do while loop(print 1 to 5)
 
-public class P07 {
+public class P08 {
 
     public static void main(String[] args) {
         int i = 1;
-        while (i <= 5) {
+        do {
             System.out.println(i);
             i++;
-        }
+        } while (i <= 5);
     }
 }
