8e8a643e173123bc2bb0de5b7213f9d7b2e95467
VARUN S M <varunsm15@gmail.com>
2026-08-06 12:22:39 +0530

    Break

---
+++ track/M02/T01/ST01/T02/ST01/P12.java
@@ -0,0 +1,13 @@
+// Break
+
+public class P12 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+            if (i == 6) {
+                break;
+            }
+        }
+    }
+}
