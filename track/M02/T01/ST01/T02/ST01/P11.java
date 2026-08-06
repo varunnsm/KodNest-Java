b868f43b761e4b2e032c42055a99aeaeffa8121c
VARUN S M <varunsm15@gmail.com>
2026-08-06 11:35:35 +0530

    Nested do-while loop

---
+++ track/M02/T01/ST01/T02/ST01/P11.java
@@ -0,0 +1,17 @@
+// Nested do-while loop
+
+public class P09 {
+
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            int j = 1;
+            do {
+                System.out.println(j);
+                j++;
+            } while (j <= 5);
+            System.out.println();
+            i++;
+        } while (i <= 5);
+    }
+}
