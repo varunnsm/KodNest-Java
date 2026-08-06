94154300e1d94e865619bea0caa4a4722b6c3b4c
VARUN S M <varunsm15@gmail.com>
2026-08-06 12:50:11 +0530

    Continue using nested for loop

--- track/M02/T01/ST01/T02/ST01/P15.java
+++ track/M02/T01/ST01/T02/ST01/P15.java
@@ -1,12 +1,12 @@
-//Break using nested for loop
+//Continue using nested for loop
 
-public class P14 {
+public class P15 {
 
     public static void main(String[] args) {
         for (int i = 1; i <= 3; i++) {
-            for (int j = 1; j <= 2; j++) {
+            for (int j = 1; j <= 4; j++) {
                 if (j == 2) {
-                    break;
+                    continue;
                 }
                 System.out.println("i:" + i + " j:" + j);
             }
