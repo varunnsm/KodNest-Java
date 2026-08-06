adf0cd5576de6ea2f3c97071a506d47088700aef
VARUN S M <varunsm15@gmail.com>
2026-08-06 10:46:20 +0530

    for loop(print 1 to 5)

---
+++ track/M02/T01/ST01/T02/ST01/P06.java
@@ -0,0 +1,10 @@
+//for loop(print 1 to 5)
+
+public class P06 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.err.println(i);
+        }
+    }
+}
