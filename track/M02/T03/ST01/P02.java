16217bf79d9fca9237e1ab24c786d7145febbad9
VARUN S M <varunsm15@gmail.com>
2026-08-07 11:52:28 +0530

    String Scanner

---
+++ track/M02/T03/ST01/P02.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class P02 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your age: ");
+        int age = sc.nextInt();
+        System.out.println("Age is " + age);
+
+        System.out.println("Enter your height: ");
+        float height = sc.nextInt();
+        System.out.println("Height is " + height);
+        sc.nextLine();
+
+        System.out.println("Enter your full name: ");
+        String name = sc.nextLine();
+        System.out.println("Full Name is " + name);
+
+    }
+}
