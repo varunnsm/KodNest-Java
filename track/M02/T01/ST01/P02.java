f753bf42994e3e8270fcf873cd9c0bfc348c5d6a
VARUN S M <varunsm15@gmail.com>
2026-08-07 13:54:02 +0530

    Convert Pseudocode to Java

---
+++ track/M02/T01/ST01/P02.java
@@ -0,0 +1,16 @@
+//Covert a Weekly Preparation Plan to Java
+
+public class P02 {
+
+    public static void main(String[] args) {
+        int javaHoursPerDay = 2;
+        int aptitudeHoursPerDay = 1;
+        int numberOfDays = 5;
+        int weeklyJavaHours = javaHoursPerDay * numberOfDays;
+        int weeklyAptitudeHours = aptitudeHoursPerDay * numberOfDays;
+        int totalPreparationHours = weeklyAptitudeHours + weeklyAptitudeHours;
+        System.out.println("Java: " + weeklyJavaHours);
+        System.out.println("Aptitude: " + weeklyAptitudeHours);
+        System.out.println("Total: " + totalPreparationHours);
+    }
+}
