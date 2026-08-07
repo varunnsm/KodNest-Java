42e386db19b23caf7ece1b579e95972b1ab4f126
VARUN S M <varunsm15@gmail.com>
2026-08-07 11:13:52 +0530

    Placement readiness Practice

---
+++ track/M02/T01/ST01/T02/ST01/P18.java
@@ -0,0 +1,17 @@
+//Placement readiness Practice
+
+public class Main {
+
+    public static void main(String[] args) {
+        int marks = 68;
+        int attendance = 80;
+        int practiceDays = 3;
+
+        // Write your code here
+        String result = (marks >= 60 && attendance >= 75) ? "Placement Ready" : "Continue Preparation";
+        System.out.println(result);
+        for (int i = 1; i <= practiceDays; i++) {
+            System.out.println("Practice Day: " + i);
+        }
+    }
+}
