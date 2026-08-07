599c5006d4f9c62a1832dda12353224c8c6b5486
VARUN S M <varunsm15@gmail.com>
2026-08-07 11:57:37 +0530

    Scanner function

---
+++ track/M02/T03/ST01/P01.java
@@ -0,0 +1,39 @@
+// Scanner
+
+import java.util.Scanner;
+
+public class P01 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter byte value: ");
+        byte a = sc.nextByte();
+        System.out.println("Byte value is " + a);
+
+        System.out.println("Enter short value: ");
+        short b = sc.nextShort();
+        System.out.println("Short value is " + b);
+
+        System.out.println("Enter integer value: ");
+        int c = sc.nextInt();
+        System.out.println("Integer value is " + c);
+
+        System.out.println("Enter long value: ");
+        long d = sc.nextLong();
+        System.out.println("Long value is " + d);
+
+        System.out.println("Enter float value: ");
+        float e = sc.nextFloat();
+        System.out.println("Float value is " + e);
+
+        System.out.println("Enter double value: ");
+        double f = sc.nextDouble();
+        System.out.println("Double value is " + f);
+
+        System.out.println("Enter boolean value: ");
+        boolean g = sc.nextBoolean();
+        System.out.println("Boolean value is " + g);
+
+    }
+}
--- track/M02/T03/ST01/P02.java
+++ track/M02/T03/ST01/P02.java
@@ -1,3 +1,4 @@
+// Scanner String Input
 
 import java.util.Scanner;
 
