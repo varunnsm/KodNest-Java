8ae2e5d1b089b9aff1e70fd6597aaa2bb10faf64
VARUN S M <varunsm15@gmail.com>
2026-08-07 14:06:37 +0530

    Declare, assign, print variables

---
+++ track/M02/T01/ST01/P03.java
@@ -0,0 +1,11 @@
+//Build Salman Learner Profile
+
+public class P03 {
+
+    public static void main(String[] args) {
+        String learnerName = "Salman";
+        int dailyPracticeHours = 8;
+        System.out.println("Name: " + learnerName);
+        System.out.println("Daily Practice: " + dailyPracticeHours + " Hours");
+    }
+}
