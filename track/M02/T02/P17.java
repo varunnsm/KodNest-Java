4f95a415995b20294e313b7ee0db0de8f1278c5f
VARUN S M <varunsm15@gmail.com>
2026-08-08 18:36:38 +0530

    Loop problems

---
+++ track/M02/T02/P17.java
@@ -0,0 +1,18 @@
+//Print and Add Selected Numbers
+
+public class Main {
+
+    public static void main(String[] args) {
+        int total = 0;
+
+        for (int number = 1; number <= 5; number++) {
+            if (number == 3) {
+                continue;
+            }
+            System.out.println("Number: " + number);
+            total += number;
+
+            System.out.println("Total: " + total);
+        }
+    }
+}
