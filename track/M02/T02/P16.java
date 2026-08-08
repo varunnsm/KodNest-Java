925df46b12be77f0b1197ee1811e31bf9111b102
VARUN S M <varunsm15@gmail.com>
2026-08-08 18:31:54 +0530

    Labeled loop using Break

---
+++ track/M02/T02/P16.java
@@ -0,0 +1,17 @@
+//Labeled loops using Break
+
+public class P16 {
+
+    public static void main(String[] args) {
+        outer:
+        for (int i = 1; i <= 3; i++) {
+            inner:
+            for (int j = 1; j <= 4; j++) {
+                if (j == 2) {
+                    break outer;
+                }
+                System.out.println("i: " + i + " " + " j:" + j);
+            }
+        }
+    }
+}
