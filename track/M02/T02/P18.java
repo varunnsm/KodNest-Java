bfc2db37bfb38612f0e2c8dbe55a0ef66b35fcb5
VARUN S M <varunsm15@gmail.com>
2026-08-08 18:42:55 +0530

    Control Flow Practice

---
+++ track/M02/T02/P18.java
@@ -0,0 +1,17 @@
+//Placement Readiness Practice (Control Flow Practice)
+
+public class Main {
+
+    public static void main(String[] args) {
+        int marks = 68;
+        int attendance = 80;
+        int practiceDays = 3;
+
+        // Write your code here
+        String result = (marks >= 60 && attendance >= 75) ? "Placement Ready" : "Continue Prepartion";
+        System.out.println(result);
+        for (int i = 1; i <= practiceDays; i++) {
+            System.out.println("Practice Day: " + i);
+        }
+    }
+}
