c17a9da3e75996aae2526ca25fad2220f0197125
VARUN S M <varunsm15@gmail.com>
2026-08-08 17:46:55 +0530

    While loop(print 1 to 5)

---
+++ track/M02/T02/P07.java
@@ -0,0 +1,12 @@
+// While loop(print 1 to 5)
+
+public class P07 {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 5) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
