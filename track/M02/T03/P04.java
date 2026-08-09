9d9663d9c9cf812d32e263d88590014e7941bf86
VARUN S M <varunsm15@gmail.com>
2026-08-09 12:32:34 +0530

    Input With Conditions and Loops

---
+++ track/M02/T03/P04.java
@@ -0,0 +1,28 @@
+// Input With Conditions and Loops
+
+import java.util.Scanner;
+
+public class P04 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int days = scanner.nextInt();
+        int solvedProblems = 0;
+        for (int i = 1; i <= days; i++) {
+            int a = scanner.nextInt();
+            solvedProblems = solvedProblems + a;
+        }
+        scanner.close();
+
+        System.out.println("Total solved: " + solvedProblems);
+        System.out.print("Status: ");
+        if (solvedProblems >= 20) {
+            System.out.println("Strong progress");
+        } else if (solvedProblems >= 10 && solvedProblems <= 19) {
+            System.out.println("Keep improving");
+        } else {
+            System.out.println("Needs more practice");
+        }
+    }
+}
