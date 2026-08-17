809ccc059c28f9baff768678d36b12bc0e7f23fe
VARUN S M <varunsm15@gmail.com>
2026-08-17 12:10:31 +0530

    First Java Program

--- track/M02 - Introduction to java/T01 - Basic programming constructs/1-firstJavaProgram.java
+++ track/M02 - Introduction to java/T01 - Basic programming constructs/1-firstJavaProgram.java
@@ -1,8 +1,8 @@
 // Creating a Java Program to Display a Message (First Java Program)
 
-public class P01 {
+public class firstJavaProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello KodNest");
+        System.out.println("Hello KodNest!");
     }
 }
