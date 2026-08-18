542ec693a8c397266f1f7587ead7b75436c800ff
VARUN S M <varunsm15@gmail.com>
2026-08-18 11:16:17 +0530

    Interactive Learner Progress Summary

--- track/M02 - Introduction to java/T03 - Console input with scanner/5-InteractiveLearnerProgressSummary.java
+++ track/M02 - Introduction to java/T03 - Console input with scanner/5-InteractiveLearnerProgressSummary.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-public class P07 {
+public class InteractiveLearnerProgressSummary {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
