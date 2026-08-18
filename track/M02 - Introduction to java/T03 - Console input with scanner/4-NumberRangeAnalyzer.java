077660ad2a359a53dc31233404e1e3d366e972c0
VARUN S M <varunsm15@gmail.com>
2026-08-18 11:13:29 +0530

    Number Range Analyzer

--- track/M02 - Introduction to java/T03 - Console input with scanner/4-NumberRangeAnalyzer.java
+++ track/M02 - Introduction to java/T03 - Console input with scanner/4-NumberRangeAnalyzer.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-public class P06 {
+public class NumberRangeAnalyzer {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
