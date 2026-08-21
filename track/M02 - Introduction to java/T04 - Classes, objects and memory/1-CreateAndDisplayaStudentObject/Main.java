930cc60b5860bc441405dd2843b5281583efee09
VARUN S M <varunsm15@gmail.com>
2026-08-21 13:08:39 +0530

    Create and Display a Student Object

---
+++ track/M02 - Introduction to java/T04 - Classes, objects and memory/1-CreateAndDisplayaStudentObject/Main.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Create one Student object
+        Student s1 = new Student();
+
+        // Read and store all values in the object
+        s1.id = sc.nextInt();
+        s1.name = sc.next();
+        s1.course = sc.next();
+        s1.javaScore = sc.nextDouble();
+
+        // Display the values stored in the object
+        s1.profile();
+
+    }
+
+}
---
+++ track/M02 - Introduction to java/T04 - Classes, objects and memory/1-CreateAndDisplayaStudentObject/Student.java
@@ -0,0 +1,17 @@
+
+class Student {
+// Declare id, name, course and javaScore
+
+    int id;
+    String name;
+    String course;
+    double javaScore;
+
+    void profile() {
+        System.out.println("Student Profile");
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Course: " + course);
+        System.out.println("Java Score: " + javaScore);
+    }
+}
