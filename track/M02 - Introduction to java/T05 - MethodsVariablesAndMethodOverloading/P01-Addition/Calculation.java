1f6b33c0311de2afa8140fbc08ea0c76e33fcc61
VARUN S M <varunsm15@gmail.com>
2026-08-22 14:10:33 +0530

    Addition

---
+++ track/M02 - Introduction to java/T05 - MethodsVariablesAndMethodOverloading/P01-Addition/Calculation.java
@@ -0,0 +1,27 @@
+
+class Calculation {
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    double add(double a, double b) {
+        return a + b;
+    }
+
+    double add(int a, double b) {
+        return a + b;
+    }
+
+    double add(double a, int b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    double add(double a, double b, double c) {
+        return a + b;
+    }
+}
---
+++ track/M02 - Introduction to java/T05 - MethodsVariablesAndMethodOverloading/P01-Addition/CalculationApplication.java
@@ -0,0 +1,10 @@
+
+public class CalApplication {
+
+    public static void main(String[] args) {
+        Calculation a1 = new Calculation();
+        System.out.println(a1.add(10, 20));
+        System.out.println(a1.add(10, 20.5));
+
+    }
+}
