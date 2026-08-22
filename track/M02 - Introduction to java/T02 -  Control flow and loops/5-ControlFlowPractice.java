226460e815d16e1e098895d73ccc037640988171
VARUN S M <varunsm15@gmail.com>
2026-08-22 15:08:45 +0530

    Placement Readiness Practice

--- track/M02 - Introduction to java/T02 -  Control flow and loops/5-ControlFlowPractice.java
+++ track/M02 - Introduction to java/T02 -  Control flow and loops/5-ControlFlowPractice.java
@@ -1,4 +1,4 @@
-//Placement Readiness Practice (Control Flow Practice)
+//Control Flow Practice(Placement Readiness Practice)
 
 public class ControlFlowPractice {
 
@@ -10,6 +10,7 @@ public class ControlFlowPractice {
         // Write your code here
         String result = (marks >= 60 && attendance >= 75) ? "Placement Ready" : "Continue Prepartion";
         System.out.println(result);
+
         for (int i = 1; i <= practiceDays; i++) {
             System.out.println("Practice Day: " + i);
         }
