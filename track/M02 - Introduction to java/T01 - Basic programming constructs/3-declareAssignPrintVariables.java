704d328f6fa5c16d08aa2e31f618928f65340fe7
VARUN S M <varunsm15@gmail.com>
2026-08-22 14:26:29 +0530

    Build Salman Learner Profile

--- track/M02 - Introduction to java/T01 - Basic programming constructs/3-declareAssignPrintVariables.java
+++ track/M02 - Introduction to java/T01 - Basic programming constructs/3-declareAssignPrintVariables.java
@@ -1,10 +1,11 @@
-// Build Salman Learner Profile (Declare, Assign, Print Variables)
+//Declare, Assign, Print Variables(Build Salman Learner Profile)
 
 public class declareAssignPrintVariables {
 
     public static void main(String[] args) {
         String learnerName = "Salman";
         int dailyPracticeHours = 8;
+
         System.out.println("Name: " + learnerName);
         System.out.println("Daily Practice: " + dailyPracticeHours + " Hours");
     }
