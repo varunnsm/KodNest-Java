7e1929613e7a7d5ff2c40d0c9c004ace86b918b5
VARUN S M <varunsm15@gmail.com>
2026-08-22 11:38:35 +0530

    Create and Call your First Method

---
+++ track/M02 - Introduction to java/T05 - MethodsVariablesAndMethodOverloading/1-CreateAndCallYourFirstMethod/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Create a Robot object
+        Robot a = new Robot();
+
+        // Call speak()
+        a.speak();
+    }
+}
---
+++ track/M02 - Introduction to java/T05 - MethodsVariablesAndMethodOverloading/1-CreateAndCallYourFirstMethod/Robot.java
@@ -0,0 +1,8 @@
+
+class Robot {
+
+    void speak() {
+        // Print the message
+        System.out.println("Beep beep! Java is my superpower!");
+    }
+}
