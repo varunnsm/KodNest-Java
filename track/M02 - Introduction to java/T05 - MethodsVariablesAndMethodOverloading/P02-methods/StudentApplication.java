17f1ee06d6d90d196625f51841799297681d7b83
VARUN S M <varunsm15@gmail.com>
2026-08-23 11:10:20 +0530

    Methods

---
+++ track/M02 - Introduction to java/T05 - MethodsVariablesAndMethodOverloading/P02-methods/StudentApplication.java
@@ -0,0 +1,9 @@
+
+public class StudentApplication {
+
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.input("Rani", 19, 4.5);
+        s1.display();
+    }
+}
