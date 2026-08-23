5844acb6a4868cdb51eebd13d25cd615cf2f2bba
VARUN S M <varunsm15@gmail.com>
2026-08-23 12:48:32 +0530

    Array Printing

---
+++ track/M02 - Introduction to java/T07 - Array foundations, memory and traversal/P02-Arrays.java
@@ -0,0 +1,13 @@
+
+public class Arrays {
+
+    public static void main(String[] args) {
+        int a[] = {10, 20, 30, 40, 50};
+
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        System.out.println(a[2]);
+        System.out.println(a[3]);
+        System.out.println(a[4]);
+    }
+}
