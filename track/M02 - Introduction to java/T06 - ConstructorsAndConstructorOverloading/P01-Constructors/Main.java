f014765a05eabf745cec809ca81e96eb4a3cdff3
VARUN S M <varunsm15@gmail.com>
2026-08-23 11:18:32 +0530

    Constructors

---
+++ track/M02 - Introduction to java/T06 - ConstructorsAndConstructorOverloading/P01-Constructors/Main.java
@@ -0,0 +1,8 @@
+
+public class StudentApplication {
+
+    public static void main(String[] args) {
+        Student s1 = new Student("Varun", 24, 5.11);
+        s1.display();
+    }
+}
---
+++ track/M02 - Introduction to java/T06 - ConstructorsAndConstructorOverloading/P01-Constructors/Student.java
@@ -0,0 +1,19 @@
+
+public class Student {
+
+    String name;
+    int age;
+    double height;
+
+    Student(String name, int age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    void display() {
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+    }
+}
