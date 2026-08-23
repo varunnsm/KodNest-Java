1cc6f0efe19da46628629256b377d019a068291b
VARUN S M <varunsm15@gmail.com>
2026-08-23 14:37:17 +0530

    Calculate Matrix Totals

---
+++ track/M02 - Introduction to java/T08 - Array problem solving and two-dimensional arrays/5-CalculateMatrixTotals.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int row = sc.nextInt();
+        int column = sc.nextInt();
+
+        int matrix[][] = new int[row][column];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < row; i++) {
+            int rowTotal = 0;
+            for (int j = 0; j < column; j++) {
+                rowTotal = rowTotal + matrix[i][j];
+            }
+            System.out.print("Row " + (i + 1) + " total: " + rowTotal);
+            System.out.println();
+        }
+    }
+}
