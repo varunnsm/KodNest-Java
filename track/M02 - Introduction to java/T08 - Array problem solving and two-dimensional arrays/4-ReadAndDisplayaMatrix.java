c78d160cbd884519874eab18ea95613f78d87f60
VARUN S M <varunsm15@gmail.com>
2026-08-23 14:12:24 +0530

    Read and Display a Matrix

---
+++ track/M02 - Introduction to java/T08 - Array problem solving and two-dimensional arrays/4-ReadAndDisplayaMatrix.java
@@ -0,0 +1,33 @@
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int rows = sc.nextInt();
+        int columns = sc.nextInt();
+
+        int arr[][] = new int[rows][columns];
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (j > 0) {
+                    System.out.print(" ");
+                }
+                System.out.print(arr[i][j]);
+            }
+
+            System.out.println();
+
+        }
+
+    }
+}
