3252f3f714ef136cc68df7ab7b465401a31da9a2
VARUN S M <varunsm15@gmail.com>
2026-09-06 23:09:07 +0530

    Find the Maximum Consecutive Ones

---
+++ track/M03 - Array Problem-Solving/T02 - Searching and Simple State Tracking/6-FindTheMaximumConsecutiveOnes.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int current = 0;
+        int max = 0;
+
+        for (int i = 0; i < size; i++) {
+            if (arr[i] == 1) {
+                current++;
+                max = Math.max(max, current);
+            } else {
+                current = 0;
+            }
+        }
+        System.out.println(max);
+    }
+}
