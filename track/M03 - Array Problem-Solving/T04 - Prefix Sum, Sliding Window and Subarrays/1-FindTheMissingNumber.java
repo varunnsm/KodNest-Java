803a16e8ae0ff6147edccb30aaff4ba663184fa0
VARUN S M <varunsm15@gmail.com>
2026-09-07 02:46:31 +0530

    Find the Missing Number

---
+++ track/M03 - Array Problem-Solving/T04 - Prefix Sum, Sliding Window and Subarrays/1-FindTheMissingNumber.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        for (int i = 0; i < size - 1; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        long expectedSum = (long) size * (size + 1) / 2;
+        long actualSum = 0;
+
+        for (int i = 0; i < size - 1; i++) {
+            actualSum = actualSum + arr[i];
+        }
+
+        System.out.println(expectedSum - actualSum);
+    }
+}
