f9763b99e1320c3fc8b39e26741bff8bc7d908cb
VARUN S M <varunsm15@gmail.com>
2026-09-07 02:23:55 +0530

    Move Negative Values to the Beginning

---
+++ track/M03 - Array Problem-Solving/T03-Two Pointers and Array Rearrangement/7-MoveNegativeValuesToTheBeginning.java
@@ -0,0 +1,41 @@
+
+import java.util.Scanner;
+
+public class MoveNegativeValuesToTheBeginning {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int left = 0;
+        int right = size - 1;
+
+        while (left < right) {
+            while (left < right && arr[left] < 0) {
+                left++;
+            }
+
+            while (left < right && arr[right] >= 0) {
+                right--;
+            }
+
+            if (left < right) {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
