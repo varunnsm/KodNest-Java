2b045fd0b9efcd821a6e3fd5b992dfbd5b43ea8a
VARUN S M <varunsm15@gmail.com>
2026-09-07 02:25:47 +0530

    Find the First Non-Repeating Value

--- track/M03 - Array Problem-Solving/T02 - Searching and Simple State Tracking/9-FindTheFirstNonRepeatingValue.java
+++ track/M03 - Array Problem-Solving/T02 - Searching and Simple State Tracking/9-FindTheFirstNonRepeatingValue.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class Main {
+public class FindTheFirstNonRepeatingValue {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
