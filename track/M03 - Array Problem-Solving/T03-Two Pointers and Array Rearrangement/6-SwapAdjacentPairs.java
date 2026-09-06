c11209cdb7c6e36bafdb11a4c6633e751c832517
VARUN S M <varunsm15@gmail.com>
2026-09-07 02:16:29 +0530

    Swap Adjacent Pairs

---
+++ track/M03 - Array Problem-Solving/T03-Two Pointers and Array Rearrangement/6-SwapAdjacentPairs.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+public class SwapAdjacentPairs {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < size; i++) {
+            int temp = 0;
+            for (int j = i + 1; j < size; j++) {
+                temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+
+                i++;
+                j++;
+                break;
+            }
+        }
+
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
