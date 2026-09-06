82c95fe9a24a2457ca6b6c877f21fbab9ab729a0
VARUN S M <varunsm15@gmail.com>
2026-09-07 01:57:41 +0530

    Separate Even and Odd Values

--- track/M03 - Array Problem-Solving/T03-Two Pointers and Array Rearrangement/2-SeparateEvenAndOddValues.java
+++ track/M03 - Array Problem-Solving/T03-Two Pointers and Array Rearrangement/2-SeparateEvenAndOddValues.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class SeparateEvenAndOddVales {
+public class SeparateEvenAndOddValues {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
