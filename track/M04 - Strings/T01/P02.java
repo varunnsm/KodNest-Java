93fd4e8c87d61e2b5bc17e30a9f6781a528a6a78
VARUN S M <varunsm15@gmail.com>
2026-09-07 17:27:34 +0530

    new String ==

---
+++ track/M04 - Strings/T01/P02.java
@@ -0,0 +1,14 @@
+
+public class P02 {
+
+    public static void main(String[] args) {
+        String s1 = new String("Java");
+        String s2 = new String("Java");
+
+        if (s1 == s2) {
+            System.out.println("References are Equal");
+        } else {
+            System.out.println("References are Not Equal");
+        }
+    }
+}
