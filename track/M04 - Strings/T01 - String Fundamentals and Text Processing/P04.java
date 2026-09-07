8a4e68210673da22ba64b64b930f7d252ee5961c
VARUN S M <varunsm15@gmail.com>
2026-09-08 01:57:27 +0530

    new String with equal()

---
+++ track/M04 - Strings/T01 - String Fundamentals and Text Processing/P04.java
@@ -0,0 +1,14 @@
+
+public class P04 {
+
+    public static void main(String[] args) {
+        String s1 = new String("Java");
+        String s2 = new String("Java");
+
+        if (s1.equals(s2)) {
+            System.out.println("Strings are Same");
+        } else {
+            System.out.println("Strings are not Same");
+        }
+    }
+}
