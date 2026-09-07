b3c8a10e99d2d18635908e94b6fcd5f71327e0e3
VARUN S M <varunsm15@gmail.com>
2026-09-08 01:48:45 +0530

    equal()

---
+++ track/M04 - Strings/T01 - String Fundamentals and Text Processing/P03.java
@@ -0,0 +1,14 @@
+
+public class P03 {
+
+    public static void main(String[] args) {
+        String s1 = "Java";
+        String s2 = "Java";
+
+        if (s1.equals(s2)) {
+            System.out.println("Strings are Same");
+        } else {
+            System.out.println("Strings are not Same");
+        }
+    }
+}
