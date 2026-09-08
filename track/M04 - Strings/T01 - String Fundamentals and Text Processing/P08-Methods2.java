a5e812d7940fa864cc059de6c7c8beb9b6d980d3
VARUN S M <varunsm15@gmail.com>
2026-09-08 17:48:33 +0530

    String Methods IMPORTANT

---
+++ track/M04 - Strings/T01 - String Fundamentals and Text Processing/P08-Methods2.java
@@ -0,0 +1,11 @@
+
+public class StringMethods2 {
+
+    public static void main(String[] args) {
+
+        String s1 = "  Java  ";
+        System.out.println(s1);
+        System.out.println(s1.trim());
+
+    }
+}
