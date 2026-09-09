6811fe7e0449be4b5032e689c5317ee1ba7fb7fb
VARUN S M <varunsm15@gmail.com>
2026-09-09 16:48:17 +0530

    String Methods IMPORTANT

---
+++ track/M04 - Strings/T01 - String Fundamentals and Text Processing/P09-Methods3.java
@@ -0,0 +1,13 @@
+
+public class StringMethods3 {
+
+    public static void main(String[] args) {
+
+        String s1 = "Java";
+        String res = s1.toUpperCase();
+
+        System.out.println(res);
+        System.out.println(s1);
+
+    }
+}
